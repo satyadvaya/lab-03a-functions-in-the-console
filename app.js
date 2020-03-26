4d51a7f93b27165938782bebd016676f80776d88
Alan Willoughby <satyadvaya@gmail.com>
2020-03-25 22:15:23 -0700

    ready to submit

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const apple = { weight: '5', color: 'red', taste: 'yummy'};
-const pear = { weight: '3', color: 'yellow', taste: 'meh'};
-const grape = { weight: '1', color: 'purple', taste: 'delish'};
-const kiwi = { weight: '2', color: 'green', taste: 'sweet'};
-const banana = { weight: '7', color: 'yellow', taste: 'bland'};
+const apple = { weight: '5', color: 'red', taste: 'yummy' };
+const pear = { weight: '3', color: 'yellow', taste: 'meh' };
+const grape = { weight: '1', color: 'purple', taste: 'delish' };
+const kiwi = { weight: '2', color: 'green', taste: 'sweet' };
+const banana = { weight: '4', color: 'yellow', taste: 'bland' };
+const orange = { weight: '6', color: 'orange', taste: 'bright' };
 
-const fruit = [apple, pear, grape, kiwi, banana];
+const fruit = [apple, pear, grape, kiwi, banana, orange];
 
 console.log(fruit);
 
-for(let i=0; i<fruit.length; i++) {
+for (let i = 0; i < fruit.length; i++) {
     const food = fruit[i];
-    console.log('weight by index', i, food.weight);
+    console.log('Weight by Index: ', i, 'yields', food.weight);
 }
+
+function logOutThings (){
+    console.log('Here are your', fruit.length, 'things' + '!')
+}
+logOutThings()
+
+function loopMove (){
+    console.log('Here are your', fruit.length, 'things' + '!')
+}
+for (let i = 0; i < fruit.length; i++) {
+    const food = fruit[i];
+    console.log(food.weight);
+}    
+loopMove()
+
+
 
 // console.log('hello world');
 // console.log('second thing')
